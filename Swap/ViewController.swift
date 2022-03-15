baabc14d1430f452fe18884e42d6b647c10da159
Daniel Belokursky <daniel.belokursky@gmail.com>
2022-03-15 14:22:38 +0300

    init: start Swap app

---
+++ Swap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swap
+//
+//  Created by Daniel Belokursky on 15.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
